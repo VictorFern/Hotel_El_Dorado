ea8b963b11edbf1df9805c19576cb16f1027d292
Faigda <jasson.rojasmadriz99@hotmail.es>
2022-04-20 15:20:55 -0600

    Se finalizo la funcionalidad del US6, con conexion a la base de datos

--- Hotel_El_Dorado/Hotel_El_Dorado/Controllers/FacilidadController.cs
+++ Hotel_El_Dorado/Hotel_El_Dorado/Controllers/FacilidadController.cs
@@ -1,14 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hotel_El_Dorado.Business;
+using Hotel_El_Dorado.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 
 namespace Hotel_El_Dorado.Controllers
 {
     public class FacilidadController : Controller
     {
+        private readonly ILogger<FacilidadController> _logger;
+        public IConfiguration Configuration { get; }
+
+        public FacilidadController(ILogger<FacilidadController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+
         public IActionResult Index()
         {
             string[,] data = { { "Imagen 1", "Texto 1"}, { "Imagen 2", "Texto 2" } };
             ViewData["data"]  = data;
+
+            PublicidadBusiness publicidadBusiness = new PublicidadBusiness(Configuration);
+            List<PublicidadModel> listaPublicidad = new List<PublicidadModel>();
+            listaPublicidad = publicidadBusiness.ObtenerPublicidad();
+            ViewBag.ListaPublicidad = listaPublicidad;
+
+            OfertaBusiness ofertaBusiness = new OfertaBusiness(Configuration);
+            List<OfertaModel> listaOferta = new List<OfertaModel>();
+            listaOferta = ofertaBusiness.ObtenerOferta();
+            ViewBag.ListaOferta = listaOferta;
             return View();
+
         }
     }
 }
