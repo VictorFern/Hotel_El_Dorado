a8fbd2578ac8834d33cfb9e261fad79a78399315
Edwin <edwinmtz87@gmail.com>
2022-04-29 13:22:07 -0600

    Añadida la funcionalidad del US11 y mejorada la US 7

---
+++ Hotel_El_Dorado/Hotel_El_Dorado/Models/ComoLlegarModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Hotel_El_Dorado.Models
+{
+    public class ComoLlegarModel
+    {
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Hotel_El_Dorado/Hotel_El_Dorado/Models/FacilidadModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Hotel_El_Dorado.Models
+{
+    public class FacilidadModel
+    {
+        public string Src { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
