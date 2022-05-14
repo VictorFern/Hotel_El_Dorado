ac2e7484a44bab1338cb5345dd965b49e11838f9
Edwin <edwinmtz87@gmail.com>
2022-05-14 17:47:36 -0600

    Código del US28

---
+++ Hotel_El_Dorado_Admin/Hotel_El_Dorado_Admin/Models/Cache.cs
@@ -0,0 +1,21 @@
+﻿namespace Hotel_El_Dorado_Admin.Models
+{
+    public sealed class Cache
+    {
+        private readonly static Cache _instance = new Cache();
+
+        public  bool isLogged { get; set; }
+        private Cache()
+        {
+            isLogged = false;
+        }
+
+        public static Cache Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+    }
+}
