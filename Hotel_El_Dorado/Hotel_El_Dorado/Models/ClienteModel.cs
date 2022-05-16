9db6047a58627af7726cca66e09eac64d08487d8
Faigda <jasson.rojasmadriz99@hotmail.es>
2022-05-15 20:14:09 -0600

    Finalizacion de la US29

--- Hotel_El_Dorado/Hotel_El_Dorado/Models/ClienteModel.cs
+++ Hotel_El_Dorado/Hotel_El_Dorado/Models/ClienteModel.cs
@@ -8,11 +8,11 @@ namespace Hotel_El_Dorado.Models
     public class ClienteModel
     {
 
-        public string Cedula { get; set; }
+        public int Cedula { get; set; }
         public string Nombre { get; set; }
         public string Apellidos { get; set; }
         public string Email { get; set; }
-        public string Tarjeta { get; set; }
+        public int Tarjeta { get; set; }
 
     }
 }
