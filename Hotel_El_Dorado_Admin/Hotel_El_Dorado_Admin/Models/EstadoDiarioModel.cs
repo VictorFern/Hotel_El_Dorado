0f612c5997599614074b5e0034bd69ac2fd0a03c
Faigda <jasson.rojasmadriz99@hotmail.es>
2022-05-27 10:42:10 -0600

    Se implemento la funcionalidad de la US22

---
+++ Hotel_El_Dorado_Admin/Hotel_El_Dorado_Admin/Models/EstadoDiarioModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel_El_Dorado_Admin.Models
+{
+    public class EstadoDiarioModel
+    {
+        public int ID_Reservacion { get; set; }
+        public string Fecha_Reservacion { get; set; }
+        public string Fecha_Entrada { get; set; }
+        public string Fecha_Salida { get; set; }
+        public HabitacionModel Habitacion { get; set; }
+        public string Estado { get; set; }
+
+        public EstadoDiarioModel()
+        {
+            Habitacion = new HabitacionModel();
+        }
+    }
+}
