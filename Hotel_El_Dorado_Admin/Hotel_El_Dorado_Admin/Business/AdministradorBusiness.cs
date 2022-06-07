d76b8fc873661cc4f3d15da62bc327b6ac355475
TommyMata <tmata24@gmail.com>
2022-06-06 21:06:34 -0700

    Crud de Ofertas y Temporadas

--- Hotel_El_Dorado_Admin/Hotel_El_Dorado_Admin/Business/AdministradorBusiness.cs
+++ Hotel_El_Dorado_Admin/Hotel_El_Dorado_Admin/Business/AdministradorBusiness.cs
@@ -17,11 +17,73 @@ namespace Hotel_El_Dorado_Admin.Business
             Configuration = configuration;
         }
 
+        public AdministradorBusiness()
+        {
+        }
+
         public List<AdministradorModel> login(AdministradorModel model)
         {
             AdministradorData administradorData = new AdministradorData(Configuration);
             return administradorData.login(model);
         }
+
+        public List<TemporadaModel> ObtenerTemporada()
+        {
+            AdministradorData administradorData = new AdministradorData(Configuration);
+            return administradorData.ObtenerTemporada();
+        }
+
+        public TemporadaModel ObtenerTemporadaID()
+        {
+            AdministradorData administradorData = new AdministradorData(Configuration);
+            return administradorData.ObtenerTemporadaID();
+        }
+
+
+        public bool guardarTemporada(TemporadaModel temporada)
+        {
+            AdministradorData adminData = new AdministradorData(Configuration);
+            return adminData.guardarTemporada(temporada);
+        }
+
+        public bool editarTemporada(TemporadaModel temporada)
+        {
+            AdministradorData adminData = new AdministradorData(Configuration);
+            return adminData.editarTemporada(temporada);
+        }
+
+        public bool eliminarTemporada(TemporadaModel temporada)
+        {
+            AdministradorData adminData = new AdministradorData(Configuration);
+            return adminData.eliminarTemporada(temporada);
+        }
+
+
+        public bool guardarOferta(OfertaModel temp)
+        {
+            AdministradorData adminData = new AdministradorData(Configuration);
+            return adminData.guardarOferta(temp);
+        }
+
+        public List<OfertaModel> ObtenerOfertas()
+        {
+            AdministradorData administradorData = new AdministradorData(Configuration);
+            return administradorData.ObtenerOfertas();
+        }
+        public bool eliminarOferta(OfertaModel temp)
+        {
+            AdministradorData adminData = new AdministradorData(Configuration);
+            return adminData.eliminarOferta(temp);
+        }
+
+        public bool editarOferta(OfertaModel temp)
+        {
+            AdministradorData adminData = new AdministradorData(Configuration);
+            return adminData.editarOferta(temp);
+        }
+
+
+
 
     }
 }
---
+++ Hotel_El_Dorado_Admin/Hotel_El_Dorado_Admin/Controllers/TemporadaController.cs
@@ -0,0 +1,65 @@
+﻿using Hotel_El_Dorado_Admin.Business;
+using Hotel_El_Dorado_Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel_El_Dorado_Admin.Controllers
+{
+    public class TemporadaController : Controller
+    {
+        public IConfiguration Configuration { get; }
+
+        public TemporadaController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IActionResult InsertarTemporada()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult guardarTemporada(TemporadaModel temp)
+        {
+            AdministradorBusiness TemBussi = new AdministradorBusiness(Configuration);
+            TemBussi.guardarTemporada(temp);
+            return RedirectToAction("VisualizarTemporada");
+        }
+
+
+        public IActionResult EliminarTemporada (TemporadaModel temp)
+        {
+            AdministradorBusiness TemBussi = new AdministradorBusiness(Configuration);
+            TemBussi.eliminarTemporada(temp);
+            return RedirectToAction("VisualizarTemporada");
+        }
+
+        public IActionResult EditarTemporada(TemporadaModel temp)
+        {
+            ViewData["item"] = temp;
+            return View();
+        }
+
+        public IActionResult ModificarTemporada(TemporadaModel temp)
+        {
+            AdministradorBusiness TemBussi = new AdministradorBusiness(Configuration);
+            TemBussi.editarTemporada(temp);
+            return RedirectToAction("VisualizarTemporada");
+        }
+
+        public IActionResult VisualizarTemporada()
+        {
+            AdministradorBusiness adminBus = new AdministradorBusiness(Configuration);
+            List<TemporadaModel> temp = new List<TemporadaModel>();
+            temp = adminBus.ObtenerTemporada();
+            return View(temp);
+        }
+
+
+    }
+}
