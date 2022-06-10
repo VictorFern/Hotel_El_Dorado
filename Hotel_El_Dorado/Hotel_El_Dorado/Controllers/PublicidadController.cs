01d85def91ab23796134bd4926aeb821af9e3e23
Faigda <jasson.rojasmadriz99@hotmail.es>
2022-06-10 11:11:57 -0600

    Se corrigió el error de la ruta de las imagenes

--- Hotel_El_Dorado/Hotel_El_Dorado/Controllers/PublicidadController.cs
+++ Hotel_El_Dorado/Hotel_El_Dorado/Controllers/PublicidadController.cs
@@ -76,7 +76,7 @@ namespace Hotel_El_Dorado.Controllers
         public async Task<String> AgregarImagen(IList<IFormFile> files)
         {
             Imagen = "";
-            var uploads = Path.Combine(_iweb.WebRootPath, "Imagenes");
+            var uploads = Path.Combine(_iweb.WebRootPath, "img");
             string ruta = "";
             foreach (var doc in files)
             {
